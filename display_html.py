c3aeb603909cb9910d99fa0333ec17f191510070
toxicwind <toxicwind@gmail.com>
2025-08-06 13:36:21 -0600

    Implement multiple code changes to enhance functionality and improve performance

--- display_html.py
+++
@@ -1,7 +0,0 @@
-from IPython.display import display, HTML
-
-def display_html_output(html_output):
-    """
-    Display the given HTML string in a Jupyter notebook.
-    """
-    display(HTML(html_output))
