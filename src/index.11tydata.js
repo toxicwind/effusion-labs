da247086a199f5aa8f68df807873096f498eb89d
toxicwind <toxicwind@gmail.com>
2025-08-15 22:16:59 -0600

    docs(homepage): provenance + ledger m/n + continuation

--- src/index.11tydata.js
+++ src/index.11tydata.js
@@ -1,12 +1,14 @@
-function pick(collection, n = 3) {
-  return (collection || []).slice(0, n);
+function takeLatest(collection, n = 3) {
+  return [...(collection || [])]
+    .sort((a, b) => b.date - a.date)
+    .slice(0, n);
 }
 
 module.exports = {
   eleventyComputed: {
-    projects: data => pick(data.collections.projects),
-    concepts: data => pick(data.collections.concepts),
-    sparks: data => pick(data.collections.sparks),
-    meta: data => pick(data.collections.meta)
+    projects: data => takeLatest(data.collections.projects),
+    concepts: data => takeLatest(data.collections.concepts),
+    sparks: data => takeLatest(data.collections.sparks),
+    meta: data => takeLatest(data.collections.meta)
   }
 };
